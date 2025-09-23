e3dad1f0ff7ec7484883182833f3514354b4a0f2
Samuel Kabuya <samuelkabuya408@gmail.com>
2025-09-23 12:00:20 -0400

    Decided to rework the entire structure again because I want to use JSX files instead of JS files. They work the same way with React anyway and I just want JSX files since instructions did'nt' specify what format I must use. I am currently in the tutorial part so I will rework page names and everything I need for the assignment later on. I need to get through the 3hr tutorial first.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Homescreen';
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
     </div>
   );
 }
